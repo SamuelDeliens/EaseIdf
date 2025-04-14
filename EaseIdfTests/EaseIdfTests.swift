1832a082d47aef905e2ed8b935474f32d84267cf
SamuelDeliens <samuel.deliens@gmail.com>
2025-04-14 15:37:30 +0200

    Initial Commit

---
+++ EaseIdfTests/EaseIdfTests.swift
@@ -0,0 +1,17 @@
+//
+//  EaseIdfTests.swift
+//  EaseIdfTests
+//
+//  Created by Samuel DELIENS on 14/04/2025.
+//
+
+import Testing
+@testable import EaseIdf
+
+struct EaseIdfTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
